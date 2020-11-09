67c23f59a795ccb221842a3ed8af9be4f890de68
RusTaggs <udachin_2000@mail.ru>
2020-11-09 16:02:59 +0300

    fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,62 +1,104 @@
+$(document).ready(function () {
+    $('.main-item').click(function(){
+        $(this).closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other
 
+        $(this).closest(".main-item").find(".offer-arrow").addClass('rotate'); // open current 
 
-$(document).ready(function(){
-    // $('.show-more').click(function(){
-    //     $(this).closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other
-        
-    //     $(this).closest(".main-item").find(".offer-arrow").addClass('rotate'); // open current 
-        
-    // });
-    
+    });
+
     //offers block
-    $('.show-more').click(function() {
+    $('.main-item').click(function () {
         var id = $(this).attr('data-tab'),
-            content = $('.offer-info[data-tab="'+ id +'"]');
-        
-        $('.show-more.active').removeClass('active');
+            content = $('.offer-info[data-tab="' + id + '"]');
+
+        $('.main-item.active').removeClass('active');
         $(this).addClass('active');
         
-        $('.offer-info.active').removeClass('active').slideUp();
-        content.slideDown().addClass('active');
-        
-        $('.close-info').click(function(){
-            content.slideUp().removeClass('active');
-        });
-     });
-    
+       
 
-     //close cookie
-    $('.close-cookie').click(function(){
-        $('.cookie').fadeOut();
+        $('.offer-info.active').removeClass('active').slideUp();
+        content.slideDown().addClass('active').css('display','flex');
+
+        $('.close-info').click(function () {
+            content.slideUp().removeClass('active');
+            $('.main-item.active').removeClass('active');
+            $('.show-more').closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other
+        });
+    });
+
+    $('.main-item.mobile').click(function () {
+        var id = $(this).attr('data-tab2'),
+            content = $('.offer-info.mobile[data-tab2="' + id + '"]');
+
+        $('.main-item.mobile.active').removeClass('active');
+        $(this).addClass('active');
+        
+       
+
+        $('.offer-info.mobile.active').removeClass('active').slideUp();
+        content.slideDown().addClass('active').css('display','flex');
+
+        $('.close-info').click(function () {
+            content.slideUp().removeClass('active');
+            $('.main-item.mobile.active').removeClass('active');
+            $('.show-more').closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other
+        });
+    });
+
+    
+   
+    
+    $('.sub-about').click(function(){
+        $('.sub-about-list').slideToggle();
+    });
+
+    $('.left-item').click(function () {
+        var id = $(this).attr('data-id'),
+            content = $('.right-item[data-id="' + id + '"]');
+
+        $('.left-item.active').removeClass('active');
+        $(this).addClass('active');
+        
+
+
+        $('.right-item.active').removeClass('active').fadeOut('fast');
+        content.fadeIn().addClass('active');
+
+        
+    });
+
+    //close cookie
+    $('.close-cookie').click(function () {
+        $('.cookie, .cookie-mobile').fadeOut();
     });
 
 
     //menu mobile fixed
-    $('.toggler').click(function(){
+    $('.toggler').click(function () {
         $('body').toggleClass('stopper');
     });
-	
 
-    
+
+
     //menu desktop
     $('.menu li').hover(function () {
-        
 
-             clearTimeout($.data(this,'timer'));
-            
-             $('ul',this).stop(true,true).fadeIn(400).css("display",'flex');
-             $('.sub', this).addClass('active');
-          }, function () {
-        
-            $.data(this,'timer', setTimeout($.proxy(function() {
-        
-              $('ul',this).stop(true,true).fadeOut(400);
-                $('.sub',this).removeClass('active');
-            }, this), 100));
-    
-          });
-    
-          
-        
+
+        clearTimeout($.data(this, 'timer'));
+
+        $('ul', this).stop(true, true).fadeIn(400).css("display", 'flex');
+        $('.sub', this).addClass('active');
+    }, function () {
+
+        $.data(this, 'timer', setTimeout($.proxy(function () {
+
+            $('ul', this).stop(true, true).fadeOut(400);
+            $('.sub', this).removeClass('active');
+        }, this), 100));
+
+    });
+
+
+
 
 });
