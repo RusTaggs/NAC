3e6aa88dbcf997b03258c1cd2318e904899b8a5f
RusTaggs <udachin_2000@mail.ru>
2020-11-11 17:14:32 +0300

    true final

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-    $('.main-item').click(function(){
+    
+    
+
+
+
+    $('.show-more').click(function(){
         $(this).closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other
 
         $(this).closest(".main-item").find(".offer-arrow").addClass('rotate'); // open current 
@@ -7,18 +12,28 @@ $(document).ready(function () {
     });
 
     //offers block
-    $('.main-item').click(function () {
+    $('.show-more').click(function () {
         var id = $(this).attr('data-tab'),
             content = $('.offer-info[data-tab="' + id + '"]');
+            
+            
 
-        $('.main-item.active').removeClass('active');
-        $(this).addClass('active');
-        
-       
+            
+                $('.main-item.active').removeClass('active');
+                $(this).parent('.main-item').addClass('active');
+                $('.offer-info.active').removeClass('active').slideUp();
+                content.slideDown().addClass('active').css('display','flex');
+               
 
-        $('.offer-info.active').removeClass('active').slideUp();
-        content.slideDown().addClass('active').css('display','flex');
+                
+                
+                
+              
+             
 
+           
+           
+            
         $('.close-info').click(function () {
             content.slideUp().removeClass('active');
             $('.main-item.active').removeClass('active');
@@ -26,24 +41,31 @@ $(document).ready(function () {
         });
     });
 
-    $('.main-item.mobile').click(function () {
+    $('.show-more.mobile').click(function () {
         var id = $(this).attr('data-tab2'),
             content = $('.offer-info.mobile[data-tab2="' + id + '"]');
-
-        $('.main-item.mobile.active').removeClass('active');
-        $(this).addClass('active');
         
-       
 
-        $('.offer-info.mobile.active').removeClass('active').slideUp();
-        content.slideDown().addClass('active').css('display','flex');
+           
+                $('.offer-info.mobile').slideUp();
+                $(this).parent('.main-item.mobile.active').removeClass('active');
+                
+                $('.show-more').closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other 
+                $('.show-more').closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other 
 
-        $('.close-info').click(function () {
-            content.slideUp().removeClass('active');
-            $('.main-item.mobile.active').removeClass('active');
-            $('.show-more').closest(".offers-main").find(".offer-arrow").removeClass('rotate'); // close all other
-        });
+                if(!$(this).next().is(':visible')){
+                    $(this).next().slideDown();
+                    $(this).parent('.main-item.mobile').addClass('active');
+                    $(this).closest(".main-item").find(".offer-arrow").addClass('rotate'); // open current 
+
+                }
+
+         
+            
+
+        
     });
+    $(".offer-info.mobile").hide();
 
     
    
